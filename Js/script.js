3d2c67d9e01c478d5574f87c7034854d0074181e
ProDrTech <prodev.xyz@gmail.com>
2025-06-04 11:18:22 +0500

    new commit

--- Js/script.js
+++ Js/script.js
@@ -5,5 +5,49 @@ jQuery(document).ready(function () {
 });
 
 window.addEventListener('DOMContentLoaded', async (e) => {
+  const wrapper = document.querySelector('[wrapper]'),
+    wrapper_item = wrapper.querySelectorAll('[div]'),
+    left_button = document.querySelector('[left]'),
+    right_button = document.querySelector('[right]'),
+    secondLeft = document.querySelector('[secondLeft]'),
+    secondRight = document.querySelector('[secondRight]');
 
+  let currentStep = 0;
+  const maxStep = wrapper_item.length - 1;
+
+  function updateTransform() {
+    wrapper_item.forEach(item => {
+      item.style.transform = `translateX(-${currentStep * 100}%)`;
+    })
+  }
+
+  right_button.addEventListener('click', () => {
+    if (currentStep < maxStep) {
+      currentStep++;
+      updateTransform();
+    }
+  });
+
+  secondRight.addEventListener('click', () => {
+    if (currentStep < maxStep) {
+      currentStep++;
+      updateTransform();
+    }
+  });
+
+  left_button.addEventListener('click', () => {
+    if (currentStep > 0) {
+      currentStep--;
+      updateTransform();
+    }
+  });
+
+  secondLeft.addEventListener('click', () => {
+    if (currentStep > 0) {
+      currentStep--;
+      updateTransform();
+    }
+  });
+
+  updateTransform();
 })
